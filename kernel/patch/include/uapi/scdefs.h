2ce0695040aad49f99d82cba553c46498f354a9a
YAIU <2146142532@qq.com>
2026-05-14 00:39:06 +0800

    change SUPERCALL_HELLO 0x1003

--- kernel/patch/include/uapi/scdefs.h
+++ kernel/patch/include/uapi/scdefs.h
@@ -20,7 +20,8 @@ static inline long hash_key(const char *key)
 // #define __NR_supercall __NR3264_truncate // 45
 #define __NR_supercall 45
 
-#define SUPERCALL_HELLO 0x1000
+// #define SUPERCALL_HELLO 0x1000
+#define SUPERCALL_HELLO 0x1003
 #define SUPERCALL_KLOG 0x1004
 
 #define SUPERCALL_BUILD_TIME 0x1007
